86808b2c8ef38abe0196b08328fc9b53b234f2ca
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-06-30 11:38:08 -0300

    Criação de trava
    Criei uma variavel de controle, para o evendo acontecer apenas depois que a animação de opacidade seja concluida

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const cartas = document.querySelectorAll(".imagens");
 let primeira_carta = false
+let trava = "desbloqueado"
 
 cartas.forEach(carta => {
     carta.addEventListener("click", () => {
-        let imagem = carta.querySelector("img");
-        if (imagem.style.opacity === "0" || imagem.style.opacity === "") {
-            imagem.style.opacity = "1";
-        } if (primeira_carta == false) {
-            primeira_carta = imagem
-        } else {
-            if (imagem.src !== primeira_carta.src) {
-                setTimeout(() => {
-                    imagem.style.opacity = "0"
-                    primeira_carta.style.opacity = "0"
-                    primeira_carta = false
-                }, 500)
+        if (trava == "desbloqueado") {
+            let imagem = carta.querySelector("img");
+            if (imagem.style.opacity === "0" || imagem.style.opacity === "") {
+                imagem.style.opacity = "1";
+            } if (primeira_carta == false) {
+                primeira_carta = imagem
             } else {
-                primeira_carta = false
+                trava = "bloqueado"
+
+                if (imagem.src !== primeira_carta.src && trava == "bloqueado") {
+                    setTimeout(() => {
+                        imagem.style.opacity = "0"
+                        primeira_carta.style.opacity = "0"
+                        primeira_carta = false
+                        trava = "desbloqueado"
+                    }, 500)
+                } else {
+                    primeira_carta = false
+                    trava = "desbloqueado"
+                }
             }
         }
     });
